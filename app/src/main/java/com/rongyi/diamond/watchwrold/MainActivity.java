839646cd4e7ae6879c74ce4852b9e0fc2854dcda
林子洋 <linziyang@rongyi.com>
2016-09-29 01:21:15 +0800

    UpDate:添加夜间模式、只实现功能，未完善配置。

--- app/src/main/java/com/rongyi/diamond/watchwrold/MainActivity.java
+++ app/src/main/java/com/rongyi/diamond/watchwrold/MainActivity.java
@@ -1,17 +1,25 @@
 package com.rongyi.diamond.watchwrold;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.rongyi.diamond.baselibiary.app.AppSpContact;
@@ -21,6 +29,7 @@ import com.rongyi.diamond.baselibiary.utils.ToastHelper;
 import java.io.File;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Author:    Diamond_Lin
@@ -43,6 +52,10 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     NavigationView mNavView;
     @Bind(R.id.drawer_layout)
     DrawerLayout mDrawerLayout;
+    @Bind(R.id.rl)
+    RelativeLayout mRl;
+    @Bind(R.id.tv_hello)
+    TextView mTvHello;
 
     @Override
     protected int getLayoutResource() {
@@ -53,7 +66,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     protected void onInitView() {
         setSupportActionBar(mToolbar);
         setNavigationView();
-//        initTheme();
+        initTheme();
     }
 
     private void setNavigationView() {
@@ -82,11 +95,11 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         }
         switch (id) {
             case R.id.nav_night:
-                Log.e("_______",(mSharedPreferencesHelper == null)+"");
                 boolean isNight = mSharedPreferencesHelper.getBoolean(AppSpContact.IS_NIGHT, false);
-                ToastHelper.showToastMessage(!isNight?"开启了夜间模式":"关闭了夜间模式");
+                ToastHelper.showToastMessage(!isNight ? "开启了夜间模式" : "关闭了夜间模式");
                 isNight = !isNight;
-                mSharedPreferencesHelper.putBoolean(AppSpContact.IS_NIGHT,isNight);
+                mSharedPreferencesHelper.putBoolean(AppSpContact.IS_NIGHT, isNight);
+                changeThemeByZhiHu();
 //                finish();
 //                startActivity(new Intent(this, MainActivity.class));
                 break;
@@ -101,5 +114,108 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         assert drawer != null;
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    //夜间模式相关
+    private void initTheme() {
+        if (mSharedPreferencesHelper.getBoolean(AppSpContact.IS_NIGHT, false)) {
+            setTheme(R.style.AppTheme);
+        } else {
+            setTheme(R.style.NightTheme);
+        }
+    }
+
+    private void changeThemeByZhiHu() {
+        showAnimation();
+        toggleThemeSetting();
+        refreshUI();
+    }
+
+    private void refreshUI() {
+        TypedValue background = new TypedValue();//背景色
+        TypedValue textColor = new TypedValue();//字体颜色
+        Resources.Theme theme = getTheme();
+        theme.resolveAttribute(R.attr.clockBackground, background, true);
+        theme.resolveAttribute(R.attr.clockTextColor, textColor, true);
+        mFab.setBackgroundResource(background.resourceId);
+        mNavView.setBackgroundResource(background.resourceId);
+        mRl.setBackgroundResource(background.resourceId);
+        Resources resources = getResources();
+        mTvHello.setTextColor(resources.getColor(textColor.resourceId));
+        refreshStatusBar();
+    }
+
+    /**
+     * 刷新 StatusBar
+     */
+    private void refreshStatusBar() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            getWindow().setStatusBarColor(getResources().getColor(typedValue.resourceId));
+        }
+    }
+
+
+    private void showAnimation() {
+        final View decorView = getWindow().getDecorView();
+        Bitmap cacheBitmap = getCacheBitmapFromView(decorView);
+        if (decorView instanceof ViewGroup && cacheBitmap != null) {
+            final View view = new View(this);
+            view.setBackgroundDrawable(new BitmapDrawable(getResources(), cacheBitmap));
+            ViewGroup.LayoutParams layoutParam = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            ((ViewGroup) decorView).addView(view, layoutParam);
+            ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(view, "alpha", 1f, 0f);
+            objectAnimator.setDuration(300);
+            objectAnimator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    ((ViewGroup) decorView).removeView(view);
+                }
+            });
+            objectAnimator.start();
+        }
+    }
+
+    /**
+     * 获取一个 View 的缓存视图
+     *
+     * @param view
+     * @return
+     */
+    private Bitmap getCacheBitmapFromView(View view) {
+        final boolean drawingCacheEnabled = true;
+        view.setDrawingCacheEnabled(drawingCacheEnabled);
+        view.buildDrawingCache(drawingCacheEnabled);
+        final Bitmap drawingCache = view.getDrawingCache();
+        Bitmap bitmap;
+        if (drawingCache != null) {
+            bitmap = Bitmap.createBitmap(drawingCache);
+            view.setDrawingCacheEnabled(false);
+        } else {
+            bitmap = null;
+        }
+        return bitmap;
+    }
+
+    /**
+     * 切换主题设置
+     */
+    private void toggleThemeSetting() {
+        if (mSharedPreferencesHelper.getBoolean(AppSpContact.IS_NIGHT, false)) {
+            setTheme(R.style.NightTheme);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
