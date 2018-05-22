4336921d2b1dfdea46efaacae316440120806507
林子洋 <anonymous_lin@foxmail.com>
2018-05-22 09:30:00 +0800

    Updated：共享 studio 配置文件

---
+++ app/src/main/java/com/rongyi/diamond/watchwrold/No.java
@@ -0,0 +1,16 @@
+package com.rongyi.diamond.watchwrold;
+
+/**
+ * Author:    Diamond_Lin
+ * Version    V1.0
+ * Date:      2017/11/21 下午5:48
+ * Description:
+ * Modification  History:
+ * Date         	Author        		Version        	Description
+ * -----------------------------------------------------------------------------------
+ * 2017/11/21      Diamond_Lin            1.0                    1.0
+ * Why & What is modified:
+ */
+
+public @interface No {
+}
